d04dcd7826e28963760a65318f221419df2c4ba3
Alice-redisdroid <itomoha494@gmail.com>
2024-11-18 12:42:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+hws_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+one_task_time = hours_spent / hws_done
+print("Курс:", course_name, ", всего задач:", hws_done, ", затрачено часов:", hours_spent, "среднее время выполнения:", one_task_time )
